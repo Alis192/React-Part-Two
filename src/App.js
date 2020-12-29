0cf188d1f8f6cafaedd8e4345b951df5cf415b98
Alis <alidov363@gmail.com>
2020-12-29 16:28:50 +0400

    second part

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes'; //importing data from
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu /> {/*To be able to use Menu component*/}
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props); //REQUIRED
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />  {/* MAKE DISHES AVAILABE AS PROPS TO THE MENU COMPONENT */}
+      </div>
+    );
+  }
 }
-
 export default App;
